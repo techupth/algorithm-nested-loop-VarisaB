c98800929c65c4ce9de8dd333354a48f59e2cbcb
Namfon <varisa.vb@hotmail.com>
2024-07-03 11:25:29 +0700

    feat: ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,13 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printStar(n) {
+  let result = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      result += "*" + "\t";
+    }
+    result += "\n";
+  }
+  return result;
 }
+
+console.log(printStar(5));
